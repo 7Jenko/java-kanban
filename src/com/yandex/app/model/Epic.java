5198981f367981f1daec40bb53312a4312739ed9
Denis 7Jenko Semizhenko <wi-q@yandex.ru>
2024-09-15 20:07:27 +0300

    sprint_8-solution-time-and-duration Добавить новые поля и методы согласно ТЗ 8.

--- src/com/yandex/app/model/Epic.java
+++ src/com/yandex/app/model/Epic.java
@@ -71,7 +71,7 @@ public class Epic extends Task {
                 "," + name +
                 "," + status +
                 "," + description +
-                "," + duration+
+                "," + duration +
                 "," + startTime;
     }
 
